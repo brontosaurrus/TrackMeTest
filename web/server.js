1c088a3d3f2a8fb2c96ba8c5512445f1c132a299
brontosaurrus <brontejurgens@gmail.com>
2019-07-09 12:26:28 +0100

    ENHANCE: add port to server.js file

--- web/server.js
+++ web/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+const base = `${__dirname}/public`;
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
